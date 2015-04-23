310931916beb066ed623504247f909244559e915
Pantazis Deligiannis <pdeligia@me.com>
2015-04-23 13:58:36 +0100

    new test and small refactoring in test dirs

---
+++ Test/Parsing/P/PLinearTopology/Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.PSharp;
+
+namespace PLinearTopology
+{
+    public class Test
+    {
+        static void Main(string[] args)
+        {
+            Runtime.RegisterNewEvent(typeof(Announce));
+            Runtime.RegisterNewEvent(typeof(Initialise));
+            Runtime.RegisterNewEvent(typeof(PowerUp));
+            Runtime.RegisterNewEvent(typeof(Local));
+            Runtime.RegisterNewEvent(typeof(StateDecisionEvent));
+            Runtime.RegisterNewEvent(typeof(Ack));
+            Runtime.RegisterNewEvent(typeof(ErBest));
+            Runtime.RegisterNewEvent(typeof(UpdateParentGM));
+            Runtime.RegisterNewEvent(typeof(goMaster));
+            Runtime.RegisterNewEvent(typeof(goSlave));
+            Runtime.RegisterNewEvent(typeof(goPassive));
+            Runtime.RegisterNewEvent(typeof(doneStateChange));
+
+            Runtime.RegisterNewMachine(typeof(GodMachine));
+            Runtime.RegisterNewMachine(typeof(Clock));
+            Runtime.RegisterNewMachine(typeof(PortMachine));
+
+            Runtime.Options.Verbose = true;
+            Runtime.Start();
+        }
+    }
+}
--- Test/Parsing/P/POpenWSN/Test.cs
+++ Test/Parsing/P/POpenWSN/Test.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.PSharp;
 
-namespace PGerman
+namespace POpenWSN
 {
     public class Test
     {
