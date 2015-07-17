05000625f7a36cc01766136a99165799273c72f6
Pantazis Deligiannis <pdeligia@me.com>
2015-07-16 23:36:49 -0700

    some unit tests for liveness

--- Source/Runtimes/BugFindingRuntime/LivenessChecker.cs
+++ Source/Runtimes/BugFindingRuntime/LivenessChecker.cs
@@ -17,7 +17,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 using Microsoft.PSharp.StateCaching;
 using Microsoft.PSharp.Tooling;
--- Source/Runtimes/BugFindingRuntime/Scheduler.cs
+++ Source/Runtimes/BugFindingRuntime/Scheduler.cs
@@ -19,7 +19,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-using Microsoft.PSharp.StateCaching;
 using Microsoft.PSharp.Tooling;
 
 namespace Microsoft.PSharp.Scheduling
