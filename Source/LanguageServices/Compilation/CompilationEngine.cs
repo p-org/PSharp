33d1ce48edd105a5b251a1e420c47fe8af8d5b68
Pantazis Deligiannis <pdeligia@me.com>
2015-08-24 17:47:42 -0700

    slight refactoring of the pdb file generator

--- Source/LanguageServices/Compilation/CompilationEngine.cs
+++ Source/LanguageServices/Compilation/CompilationEngine.cs
@@ -194,18 +194,19 @@ namespace Microsoft.PSharp.LanguageServices.Compilation
             {
                 outputDirectory = Path.GetDirectoryName(outputPath);
             }
+
             CompilationEngine.OutputDirectoryMap.Add(compilation.AssemblyName, outputDirectory);
 
-            string fileName = outputDirectory + Path.DirectorySeparatorChar + assemblyFileName,
-                pdbFileName = outputDirectory + Path.DirectorySeparatorChar + compilation.AssemblyName + ".pdb";
+            string fileName = outputDirectory + Path.DirectorySeparatorChar + assemblyFileName;
+            string pdbFileName = outputDirectory + Path.DirectorySeparatorChar + compilation.AssemblyName + ".pdb";
             CompilationEngine.ProjectAssemblyPathMap.Add(compilation.AssemblyName, fileName);
 
             // Link external references.
             CompilationEngine.LinkExternalAssembliesToProject(compilation);
 
             EmitResult emitResult = null;
-            using (var outputFile = new FileStream(fileName, FileMode.Create, FileAccess.Write))
-            using (var outputPdbFile = new FileStream(pdbFileName, FileMode.Create, FileAccess.Write))
+            using (FileStream outputFile = new FileStream(fileName, FileMode.Create, FileAccess.Write),
+                outputPdbFile = new FileStream(pdbFileName, FileMode.Create, FileAccess.Write))
             {
                 emitResult = compilation.Emit(outputFile, outputPdbFile);
                 if (emitResult.Success)
@@ -266,24 +267,6 @@ namespace Microsoft.PSharp.LanguageServices.Compilation
 
         #region linking methods
 
-        private static void LinkAssembly(string src, string dest)
-        {
-            File.Delete(dest);
-            File.Copy(src, dest);
-
-            if (src.EndsWith(".dll") && dest.EndsWith(".dll"))
-            {
-                string srcPdb = src.Substring(0, src.Length - 4) + ".pdb",
-                    destPdb = dest.Substring(0, dest.Length - 4) + ".pdb";
-
-                if (File.Exists(srcPdb))
-                {
-                    File.Delete(destPdb);
-                    File.Copy(srcPdb, destPdb);
-                }
-            }
-        }
-
         /// <summary>
         /// Links the solution project assemblies to the given P# project.
         /// </summary>
@@ -299,7 +282,7 @@ namespace Microsoft.PSharp.LanguageServices.Compilation
                 var assemblyPath = CompilationEngine.ProjectAssemblyPathMap[requiredProject.AssemblyName];
                 var fileName = projectPath + Path.DirectorySeparatorChar + requiredProject.AssemblyName + ".dll";
 
-                LinkAssembly(assemblyPath, fileName);
+                CompilationEngine.CopyAssembly(assemblyPath, fileName);
             }
         }
 
@@ -332,7 +315,30 @@ namespace Microsoft.PSharp.LanguageServices.Compilation
                 var localFileName = (new System.Uri(assembly.CodeBase)).LocalPath;
                 var fileName = outputDir + Path.DirectorySeparatorChar + dll;
 
-                LinkAssembly(localFileName, fileName);
+                CompilationEngine.CopyAssembly(localFileName, fileName);
+            }
+        }
+
+        /// <summary>
+        /// Copies the assembly from the source to the destination.
+        /// </summary>
+        /// <param name="src">Source</param>
+        /// <param name="dest">Destination</param>
+        private static void CopyAssembly(string src, string dest)
+        {
+            File.Delete(dest);
+            File.Copy(src, dest);
+
+            if (src.EndsWith(".dll") && dest.EndsWith(".dll"))
+            {
+                string srcPdb = src.Substring(0, src.Length - 4) + ".pdb",
+                    destPdb = dest.Substring(0, dest.Length - 4) + ".pdb";
+
+                if (File.Exists(srcPdb))
+                {
+                    File.Delete(destPdb);
+                    File.Copy(srcPdb, destPdb);
+                }
             }
         }
 
