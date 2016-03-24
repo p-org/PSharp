0cc9cbf17ae1fecd74674fcbe21fc58bcd104848
Rashmi <t-rasmud@microsoft.com>
2016-03-24 16:07:01 +0530

    added Raft and ReplicatingStorage to PSharpAsLanguage

---
+++ Samples/PSharpAsLanguage/Raft/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.PSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raft
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var runtime = PSharpRuntime.Create();
+            //Program.Execute(runtime);
+            Console.ReadLine();
+        }
+
+        [Microsoft.PSharp.Test]
+        public static void Execute(PSharpRuntime runtime)
+        {
+            //runtime.CreateMachine(typeof(ClusterManager));
+        }
+    }
+}
