87285f5c1c18ce9c0354f87d3a00a56ca77d9969
Pantazis Deligiannis <pdeligia@me.com>
2016-03-24 14:52:12 +0000

    new static analysis unit tests

--- Test/StaticAnalysis.Tests.Unit/AccessChecking/StaticError/WriteAccessAfterSendFailTests.cs
+++ Test/StaticAnalysis.Tests.Unit/AccessChecking/StaticError/WriteAccessAfterSendFailTests.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="AccessAfterSendFailTests.cs">
+// <copyright file="WriteAccessAfterSendFailTests.cs">
 //      Copyright (c) Microsoft Corporation. All rights reserved.
 // 
 //      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
@@ -24,10 +24,10 @@ using Microsoft.PSharp.Utilities;
 namespace Microsoft.PSharp.StaticAnalysis.Tests.Unit
 {
     [TestClass]
-    public class AccessAfterSendFailTests : BasePSharpTest
+    public class WriteAccessAfterSendFailTests : BasePSharpTest
     {
         [TestMethod, Timeout(3000)]
-        public void TestAccessAfterSend1Fail()
+        public void TestWriteAccessAfterSend1Fail()
         {
             var test = @"
 using Microsoft.PSharp;
@@ -103,7 +103,7 @@ class M : Machine
         }
 
         [TestMethod, Timeout(3000)]
-        public void TestAccessAfterSend2Fail()
+        public void TestWriteAccessAfterSend2Fail()
         {
             var test = @"
 using Microsoft.PSharp;
@@ -179,7 +179,84 @@ class M : Machine
         }
 
         [TestMethod, Timeout(3000)]
-        public void TestAccessAfterSendInCalleeFail()
+        public void TestWriteAccessAfterSendLoadFromFieldFail()
+        {
+            var test = @"
+using Microsoft.PSharp;
+
+namespace Foo {
+class eUnit : Event
+{
+ public Letter Letter;
+ 
+ public eUnit(Letter letter)
+  : base()
+ {
+  this.Letter = letter;
+ }
+}
+
+struct Letter
+{
+ public string Text;
+
+ public Letter(string text)
+ {
+  this.Text = text;
+ }
+}
+
+class M : Machine
+{
+ MachineId Target;
+ string Text;
+
+ [Start]
+ [OnEntry(nameof(FirstOnEntryAction))]
+ class First : MachineState { }
+
+ void FirstOnEntryAction()
+ {
+  this.Target = this.CreateMachine(typeof(M));
+  var letter = new Letter(""test"");
+  this.Send(this.Target, new eUnit(letter));
+  letter.Text = this.Text;
+ }
+}
+}";
+
+            var solution = base.GetSolution(test);
+
+            var configuration = Configuration.Create();
+            configuration.ProjectName = "Test";
+            configuration.Verbose = 2;
+
+            IO.StartWritingToMemory();
+
+            var context = CompilationContext.Create(configuration).LoadSolution(solution);
+
+            ParsingEngine.Create(context).Run();
+            RewritingEngine.Create(context).Run();
+
+            AnalysisErrorReporter.ResetStats();
+            StaticAnalysisEngine.Create(context).Run();
+
+            var stats = AnalysisErrorReporter.GetStats();
+            var expected = "... Static analysis detected '1' error";
+            Assert.AreEqual(expected.Replace(Environment.NewLine, string.Empty), stats);
+
+            var error = "Error: Method 'FirstOnEntryAction' of machine 'Foo.M' " +
+                "accesses 'letter' after giving up its ownership.";
+            var actual = IO.GetOutput();
+
+            Assert.AreEqual(error.Replace(Environment.NewLine, string.Empty),
+               actual.Substring(0, actual.IndexOf(Environment.NewLine)));
+
+            IO.StopWritingToMemory();
+        }
+
+        [TestMethod, Timeout(3000)]
+        public void TestWriteAccessAfterSendInCalleeFail()
         {
             var test = @"
 using Microsoft.PSharp;
