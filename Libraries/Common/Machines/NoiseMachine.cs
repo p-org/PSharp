a20fd97ebf0170a1148fd348d877bd890ff4368b
Pantazis Deligiannis <pdeligia@me.com>
2016-04-23 11:30:39 +0100

    added library project with common machines

---
+++ Libraries/Common/Machines/NoiseMachine.cs
@@ -0,0 +1,65 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="NoiseMachine.cs">
+//      Copyright (c) Microsoft Corporation. All rights reserved.
+// 
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//      IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//      CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//      TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//      SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Microsoft.PSharp.Common
+{
+    internal class NoiseMachine : Machine
+    {
+        internal class ConfigureEvent : Event
+        {
+            public MachineId Sender;
+            public int Duration;
+
+            public ConfigureEvent(MachineId sender, int duration)
+                : base()
+            {
+                this.Sender = sender;
+                this.Duration = duration;
+            }
+        }
+
+        internal class Done : Event { }
+        private class NoiseEvent : Event { }
+
+        private MachineId Sender;
+        private int Duration;
+
+        [Start]
+        [OnEntry(nameof(InitOnEntry))]
+        class Init : MachineState { }
+
+        void InitOnEntry()
+        {
+            this.Sender = (this.ReceivedEvent as ConfigureEvent).Sender;
+            this.Duration = (this.ReceivedEvent as ConfigureEvent).Duration;
+            this.Goto(typeof(Active));
+        }
+
+        [OnEntry(nameof(ActiveOnEntry))]
+        [OnEventGotoState(typeof(NoiseEvent), typeof(Active))]
+        class Active : MachineState { }
+
+        void ActiveOnEntry()
+        {
+            this.Send(this.Id, new NoiseEvent());
+            this.Duration--;
+
+            if (this.Duration <= 0)
+            {
+                this.Send(this.Sender, new Done());
+                this.Raise(new Halt());
+            }
+        }
+    }
+}
