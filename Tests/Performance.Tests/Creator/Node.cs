70a6c135b19cd15deaa863f9f1694d1b1a50f78c
Pantazis Deligiannis <pdeligia@me.com>
2016-06-15 14:29:32 +0530

    added a performance test

---
+++ Tests/Performance.Tests/Creator/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using Microsoft.PSharp;
+
+namespace Creator
+{
+    internal class Node : Machine
+    {
+        [Start]
+        [OnEntry(nameof(InitOnEntry))]
+        class Init : MachineState { }
+
+        void InitOnEntry()
+        {
+            this.Execute();
+        }
+
+        void Execute()
+        {
+            Thread.SpinWait(50);
+        }
+    }
+}
---
+++ Tests/Performance.Tests/Creator/Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Microsoft.PSharp;
+using Microsoft.PSharp.Utilities;
+
+namespace Creator
+{
+    public class Test
+    {
+        static readonly int NumOfNodes = 100000;
+
+        static void Main(string[] args)
+        {
+            Profiler profiler = new Profiler();
+            profiler.StartMeasuringExecutionTime();
+
+            var runtime = PSharpRuntime.Create();
+            Test.Execute(runtime);
+            runtime.Wait();
+
+            profiler.StopMeasuringExecutionTime();
+            Console.WriteLine("... P# executed for '" +
+                profiler.Results() + "' seconds.");
+
+            profiler.StartMeasuringExecutionTime();
+
+            new TPLTest().Start(Test.NumOfNodes);
+
+            profiler.StopMeasuringExecutionTime();
+            Console.WriteLine("... TPL executed for '" +
+                profiler.Results() + "' seconds.");
+        }
+
+        [Microsoft.PSharp.Test]
+        public static void Execute(PSharpRuntime runtime)
+        {
+            runtime.CreateMachine(typeof(Environment),
+                new Environment.Config(Test.NumOfNodes));
+        }
+    }
+}
