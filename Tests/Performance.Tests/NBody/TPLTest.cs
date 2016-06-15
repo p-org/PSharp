b0860409c5bd615fd71f9c7c306d3dc8c1f9d1e5
Pantazis Deligiannis <pdeligia@me.com>
2016-06-15 16:32:49 +0530

    added nbody performance benchmark

---
+++ Tests/Performance.Tests/NBody/TPLTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NBody
+{
+    public class TPLTest
+    {
+        private ConcurrentBag<Body> Bodies;
+
+        private readonly double TimeStep = 0.001;
+        private readonly double EPS = 0.01;
+
+        public void Start(int numOfBodies, int steps)
+        {
+            this.Bodies = new ConcurrentBag<Body>();
+            this.GenerateBodies(numOfBodies);
+            
+            Console.WriteLine("{0:f9}", this.ComputeEnergy());
+
+            for (int i = 0; i < steps; i++)
+            {
+                this.DoStep();
+            }
+            
+            Console.WriteLine("{0:f9}", this.ComputeEnergy());
+        }
+
+        public void DoStep()
+        {
+            Parallel.ForEach(this.Bodies, (bi) =>
+            {
+                foreach (Body bj in this.Bodies)
+                {
+                    if (bi == bj)
+                    {
+                        continue;
+                    }
+
+                    double dx = bi.X - bj.X;
+                    double dy = bi.Y - bj.Y;
+                    double dz = bi.Z - bj.Z;
+
+                    double dSquared = dx * dx + dy * dy + dz * dz + this.EPS;
+                    double distance = Math.Sqrt(dSquared);
+                    double mag = this.TimeStep / (dSquared * distance);
+                    double mmag = bj.Mass * mag;
+
+                    bi.VX -= dx * bj.Mass * mag;
+                    bi.VY -= dy * bj.Mass * mag;
+                    bi.VZ -= dz * bj.Mass * mag;
+                }
+            });
+
+            Parallel.ForEach(this.Bodies, (body) =>
+            {
+                body.X += this.TimeStep * body.VX;
+                body.Y += this.TimeStep * body.VY;
+                body.Z += this.TimeStep * body.VZ;
+            });
+        }
+
+        public double ComputeEnergy()
+        {
+            double res = 0.0;
+
+            foreach (var body in this.Bodies)
+            {
+                res += body.X + body.Y + body.Z + body.VX + body.VY + body.VZ + body.Mass;
+            }
+
+            return res;
+        }
+
+        private void GenerateBodies(int numOfBodies)
+        {
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            for (int idx = 0; idx < numOfBodies; idx++)
+            {
+                this.Bodies.Add(new Body(rnd.NextDouble(), rnd.NextDouble(), rnd.NextDouble(),
+                    rnd.NextDouble(), rnd.NextDouble(), rnd.NextDouble(), rnd.NextDouble()));
+            }
+        }
+    }
+}
---
+++ Tests/Performance.Tests/NBody/Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Microsoft.PSharp;
+using Microsoft.PSharp.Utilities;
+
+namespace NBody
+{
+    public class Test
+    {
+        static readonly int NumOfBodies = 100;
+        static readonly int NumOfSteps = 100;
+
+        static void Main(string[] args)
+        {
+            Profiler profiler = new Profiler();
+            profiler.StartMeasuringExecutionTime();
+
+            var runtime = PSharpRuntime.Create();
+            Test.Execute(runtime);
+            runtime.Wait();
+
+            profiler.StopMeasuringExecutionTime();
+            Console.WriteLine("... P# executed for '" +
+                profiler.Results() + "' seconds.");
+
+            //profiler.StartMeasuringExecutionTime();
+
+            //new TPLTest().Start(Test.NumOfBodies, Test.NumOfSteps);
+
+            //profiler.StopMeasuringExecutionTime();
+            //Console.WriteLine("... TPL executed for '" +
+            //    profiler.Results() + "' seconds.");
+        }
+
+        [Microsoft.PSharp.Test]
+        public static void Execute(PSharpRuntime runtime)
+        {
+            runtime.CreateMachine(typeof(Simulation),
+                new Simulation.Config(Test.NumOfBodies, Test.NumOfSteps));
+        }
+    }
+}
