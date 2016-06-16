5d019bde7632c26d753795fca008bf1ce96a7e8a
Pantazis Deligiannis <pdeligia@me.com>
2016-06-16 16:40:30 +0530

    fix in references

--- AddOns/DynamicRaceDetection/RaceDetector/RaceDetectionProcess.cs
+++ AddOns/DynamicRaceDetection/RaceDetector/RaceDetectionProcess.cs
@@ -13,7 +13,6 @@
 //-----------------------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Reflection;
