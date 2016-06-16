c76a0bdb4d06da0bba57036b067c3b7d6deb67fc
Rashmi <t-rasmud@microsoft.com>
2016-06-16 17:17:15 +0530

    Async await state machine

---
+++ Samples/Experimental/AsyncAwait/AsyncAwaitStateMachine/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitStateMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncClass obj = new AsyncClass();
+            obj.caller();
+        }
+    }
+
+    class AsyncClass
+    {
+        public void caller()
+        {
+            Task<int> resultTask = this.fooAsync(20);
+            Console.WriteLine("Inside caller");
+            resultTask.Wait();
+        }
+
+        public /*async*/ Task<int> fooAsync(int x)
+        {
+            StateMachine stateMachine = new StateMachine(x);
+            stateMachine.MoveNext();
+            return stateMachine.resultTask.Task;
+        }
+    }
+}
