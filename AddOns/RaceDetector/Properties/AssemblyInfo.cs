d4564328f5b32d004129a8251d2be2cf1fd64e14
Pantazis Deligiannis <pdeligia@me.com>
2016-06-16 13:52:09 +0530

    more refactoring

--- AddOns/RaceDetector/Properties/AssemblyInfo.cs
+++ AddOns/RaceDetector/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.PSharp.DynamicRaceDetection")]
+[assembly: AssemblyTitle("PSharpRaceDetector")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Microsoft.PSharp.DynamicRaceDetection")]
+[assembly: AssemblyProduct("PSharpRaceDetector")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
