0955812a65d7c5210d07dfa8cc900dcbafde84b0
Pantazis Deligiannis <pdeligia@me.com>
2016-07-08 11:05:05 +0530

    renamed to TraceViewer

--- Tools/Testing/TraceViewer/Properties/AssemblyInfo.cs
+++ Tools/Testing/TraceViewer/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PSharpTraceVisualizer")]
+[assembly: AssemblyTitle("PSharpTraceViewer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft Corporation")]
-[assembly: AssemblyProduct("PSharpTraceVisualizer")]
+[assembly: AssemblyProduct("PSharpTraceViewer")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
