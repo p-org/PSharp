d2268aee335833f5b3f6e023a79a9cdbf795fb36
Rashmi <t-rasmud@microsoft.com>
2016-10-27 15:10:27 +0530

    Added a sample

---
+++ Samples/Experimental/RaceTest/RaceTest/Client.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.PSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTest
+{
+    public class Client : Machine
+    {
+        public class Ping : Event
+        {
+            public int Count;
+
+            public Ping(int count)
+            {
+                this.Count = count;
+            }
+        }
+
+        [Start]
+        [OnEventDoAction(typeof(Ping), nameof(OnPing))]
+        class Init : MachineState { }
+
+        void OnPing()
+        {
+            var e = ReceivedEvent as Ping;
+            Console.WriteLine(e.Count);
+        }
+    }
+}
---
+++ Samples/Experimental/RaceTest/RaceTest/Server.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.PSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTest
+{
+    public class Server : Machine
+    {
+        [Start]
+        [OnEntry(nameof(OnInint))]
+        class Init : MachineState { }
+
+        void OnInint()
+        {
+            var client = CreateMachine(typeof(Client));
+            var ev = new Client.Ping(5);
+            Send(client, ev);
+            ev.Count++;
+        }
+    }
+}
