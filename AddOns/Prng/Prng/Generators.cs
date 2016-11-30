2345f6d9ba1026c868fb7f49b1f026447d7aa817
Akash Lal <akashl@microsoft.com>
2016-11-30 19:55:44 +0530

    Creating an AddOn to test Random number generation

---
+++ AddOns/Prng/Prng/Generators.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prng
+{
+    interface IGenerator
+    {
+        string Name();
+
+        int Next();
+
+        int Next(int low, int high);
+    }
+
+    class RandomWrapper : IGenerator
+    {
+        Random rnd;
+
+        public string Name()
+        {
+            return "System.Random";
+        }
+
+        public RandomWrapper()
+        {
+            rnd = new Random();
+        }
+
+        public int Next()
+        {
+            return rnd.Next();
+        }
+
+        public int Next(int low, int high)
+        {
+            return rnd.Next(low, high);
+        }
+    }
+}
---
+++ AddOns/Prng/Prng/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prng
+{
+    class Program
+    {
+        public static readonly int N = 1000000;
+
+        static void Main(string[] args)
+        {
+            Run(new RandomWrapper());
+        }
+
+        static void Run(IGenerator gen)
+        {
+            var sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            var dist = GetDistribution(gen);
+
+            sw.Stop();
+            Console.WriteLine("Time for {0}: {1}", gen.Name(), sw.Elapsed.TotalSeconds.ToString("F5"));
+
+            var buckets = GetBuckets(dist, 10);
+            foreach (var tup in buckets)
+            {
+                Console.WriteLine("Count for bucket {0} for {1}: {2}%", tup.Key, gen.Name(), (tup.Value * 100.0 / N).ToString("F2"));
+            }
+        }
+
+
+        static List<int> GetDistribution(IGenerator gen)
+        {
+            var ret = new List<int>();
+            for (int i = 0; i < N; i++)
+            {
+                ret.Add(gen.Next());
+            }
+            return ret;
+        }
+
+        static Dictionary<int, int> GetBuckets(List<int> dist, int n)
+        {
+            var ret = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                ret.Add(i, 0);
+            }
+
+            foreach(var x in dist)
+            {
+                ret[x % n]++;
+            }
+
+            return ret;
+        }
+
+    }
+
+
+
+}
