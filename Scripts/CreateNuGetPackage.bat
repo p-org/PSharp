b3bd8e70ba55277fe11c161887113a13eddbe79f
Pantazis Deligiannis <pdeligia@microsoft.com>
2017-01-17 21:41:53 +0000

    CreateNuGetPackage script now downloads the NuGet.exe tool if not available

--- Scripts/CreateNuGetPackage.bat
+++ Scripts/CreateNuGetPackage.bat
@@ -1,7 +1,11 @@
 @echo off
 if not exist .\NuGet\nuget.exe (
-    echo Error: Cannot detect 'nuget.exe' tool in '.\NuGet\' directory
-    exit
+    echo . Downloading 'nuget.exe' tool
+    powershell -command Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile ".\NuGet\nuget.exe"
+    if not exist .\NuGet\nuget.exe (
+        echo Error: Unable to download 'nuget.exe'. Please download from "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" and place in 'PSharp\Scripts\NuGet\' directory.
+        exit
+    )
 )
 
 echo . Copying 'PSharp.nuspec' to '..\Binaries' directory
