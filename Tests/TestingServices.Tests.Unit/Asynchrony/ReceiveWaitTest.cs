4f624f02aca685eefd45a0f8c9d470545401168d
Akash Lal <akashl@microsoft.com>
2017-04-20 21:39:43 +0530

    Added a test for Receive.Wait

---
+++ Tests/TestingServices.Tests.Unit/Asynchrony/ReceiveWaitTest.cs
@@ -0,0 +1,52 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ReceiveWaitTest.cs">
+//      Copyright (c) Microsoft Corporation. All rights reserved.
+// 
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//      IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//      CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//      TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//      SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace Microsoft.PSharp.TestingServices.Tests.Unit
+{
+    public class ReceiveWaitTest : BaseTest
+    {
+        class E : Event { }
+
+        class M : Machine
+        {
+            [Start]
+            [OnEntry(nameof(EntryInit))]
+            class Init : MachineState { }
+
+            void EntryInit()
+            {
+                this.Send(this.Id, new E());
+                this.Receive(typeof(E)).Wait();
+                this.Assert(false);
+            }
+        }
+
+        [Fact]
+        public void TestAsyncReceiveEvent()
+        {
+            var test = new Action<PSharpRuntime>((r) => {
+                r.CreateMachine(typeof(M));
+            });
+
+            var bugReport = "Detected an assertion failure.";
+            base.AssertFailed(test, bugReport);
+        }
+    }
+}
