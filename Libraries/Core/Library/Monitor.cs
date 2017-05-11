c2ca66f010544d73ed1a87c7080dea226b959ade
t-rasmud <t-rasmud@microsoft.com>
2017-05-11 23:08:56 +0530

    changed hash in monitor as well

--- Libraries/Core/Library/Monitor.cs
+++ Libraries/Core/Library/Monitor.cs
@@ -535,11 +535,11 @@ namespace Microsoft.PSharp
             {
                 var hash = 19;
 
-                hash = hash + 31 * this.GetType().GetHashCode();
-                hash = hash + 31 * this.CurrentState.GetHashCode();
+                hash = hash * 31 + this.GetType().GetHashCode();
+                hash = hash * 31 + this.CurrentState.GetHashCode();
 
                 // Adds the user-defined hashed state.
-                hash = hash + 31 * this.GetHashedState(); 
+                hash = hash * 31 + this.GetHashedState(); 
 
                 return hash;
             }
