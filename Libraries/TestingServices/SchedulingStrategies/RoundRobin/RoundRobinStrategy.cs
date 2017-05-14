de08c1b6affdf47ba487be96ca673d8430ef8a83
t-rasmud <t-rasmud@microsoft.com>
2017-05-14 22:04:14 +0530

    RR with random delays

--- Libraries/TestingServices/SchedulingStrategies/RoundRobin/RoundRobinStrategy.cs
+++ Libraries/TestingServices/SchedulingStrategies/RoundRobin/RoundRobinStrategy.cs
@@ -42,14 +42,16 @@ namespace Microsoft.PSharp.TestingServices.Scheduling
         private IRandomNumberGenerator Random;
 
         /// <summary>
-        /// List of prioritized machines.
-        /// </summary>
-        private List<MachineId> MachineQueue;
-
-        /// <summary>
         /// The round robin index.
         /// </summary>
         private int RoundRobinIdx;
+
+        /// <summary>
+        /// Set of priority change points.
+        /// </summary>
+        private SortedSet<int> DelayPoints;
+
+        private List<MachineId> DelayQ;
 
         #endregion
 
@@ -66,8 +68,9 @@ namespace Microsoft.PSharp.TestingServices.Scheduling
             this.ExploredSteps = 0;
             this.Seed = this.Configuration.RandomSchedulingSeed ?? DateTime.Now.Millisecond;
             this.Random = new DefaultRandomNumberGenerator(this.Seed);
-            this.MachineQueue = new List<MachineId>();
             this.RoundRobinIdx = -1;
+            this.DelayPoints = new SortedSet<int>();
+            this.DelayQ = new List<MachineId>();
         }
 
         /// <summary>
@@ -91,18 +94,12 @@ namespace Microsoft.PSharp.TestingServices.Scheduling
                 }
             }
 
-            //var tempRoundRobinIdx = (RoundRobinIdx + 1) % (choices.Count());
-            //while (!choices.ElementAt(tempRoundRobinIdx).IsEnabled || choices.ElementAt(tempRoundRobinIdx).IsWaitingToReceive)
-            //{
-            //    tempRoundRobinIdx++;
-            //}
-            //RoundRobinIdx = tempRoundRobinIdx;
-            //next = choices.ElementAt(RoundRobinIdx);
-
-            //Console.WriteLine("Scheduling Machine: " + next.Id);
-            RoundRobinIdx = (RoundRobinIdx + 1) % availableMachines.Count;
+            if(!DelayPoints.Contains(this.ExploredSteps))
+                RoundRobinIdx = (RoundRobinIdx + 1) % availableMachines.Count;
+            else
+                RoundRobinIdx = (RoundRobinIdx + 2) % availableMachines.Count;
             next = availableMachines[RoundRobinIdx];
-
+            
             this.ExploredSteps++;
 
             return true;
@@ -193,12 +190,17 @@ namespace Microsoft.PSharp.TestingServices.Scheduling
             this.MaxExploredSteps = Math.Max(this.MaxExploredSteps, this.ExploredSteps);
             this.ExploredSteps = 0;
 
-            this.MachineQueue.Clear();
+            this.DelayPoints.Clear();
 
             var range = new List<int>();
             for (int idx = 0; idx < this.MaxExploredSteps; idx++)
             {
                 range.Add(idx);
+            }
+
+            foreach (int point in this.Shuffle(range).Take(10))
+            {
+                this.DelayPoints.Add(point);
             }
         }
 
@@ -210,7 +212,7 @@ namespace Microsoft.PSharp.TestingServices.Scheduling
             this.MaxExploredSteps = 0;
             this.ExploredSteps = 0;
             this.Random = new DefaultRandomNumberGenerator(this.Seed);
-            this.MachineQueue.Clear();
+            this.DelayPoints.Clear();
         }
 
         /// <summary>
@@ -223,5 +225,25 @@ namespace Microsoft.PSharp.TestingServices.Scheduling
         }
 
         #endregion
+
+        /// <summary>
+        /// Shuffles the specified list using the
+        /// Fisher-Yates algorithm.
+        /// </summary>
+        /// <param name="list">IList</param>
+        /// <returns>IList</returns>
+        private IList<int> Shuffle(IList<int> list)
+        {
+            var result = new List<int>(list);
+            for (int idx = result.Count - 1; idx >= 1; idx--)
+            {
+                int point = this.Random.Next(this.MaxExploredSteps);
+                int temp = result[idx];
+                result[idx] = result[point];
+                result[point] = temp;
+            }
+
+            return result;
+        }
     }
 }
