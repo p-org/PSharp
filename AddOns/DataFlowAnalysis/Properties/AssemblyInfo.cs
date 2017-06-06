eb4745ec5abb4ee00f977891015298adead0c61d
Pantazis Deligiannis <pdeligia@microsoft.com>
2017-06-06 10:42:16 +0100

    updated version

--- AddOns/DataFlowAnalysis/Properties/AssemblyInfo.cs
+++ AddOns/DataFlowAnalysis/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.2.2.0")]
+[assembly: AssemblyFileVersion("1.2.2.0")]
 
 [assembly: InternalsVisibleTo("PSharpRemoteManager,PublicKey=" +
     "0024000004800000940000000602000000240000525341310004000001000100d7971281941569" +
