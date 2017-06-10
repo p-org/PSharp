d5efcd969b942e804d918b170db22a0a39a171a0
Pantazis Deligiannis <pdeligia@microsoft.com>
2017-06-10 13:06:20 +0100

    added two unit tests

---
+++ Tests/TestingServices.Tests.Unit/Machines/Declarations/AmbiguousEventHandlerTest.cs
@@ -0,0 +1,77 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AmbiguousEventHandlerTest.cs">
+//      Copyright (c) Microsoft Corporation. All rights reserved.
+// 
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//      IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//      CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//      TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//      SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+
+using Xunit;
+
+namespace Microsoft.PSharp.TestingServices.Tests.Unit
+{
+    public class AmbiguousEventHandlerTest : BaseTest
+    {
+        class E : Event { }
+
+        class Program : Machine
+        {
+            [Start]
+            [OnEntry(nameof(InitOnEntry))]
+            [OnEventDoAction(typeof(E), nameof(HandleE))]
+            public class Init : MachineState { }
+
+            void InitOnEntry()
+            {
+                this.Send(this.Id, new E());
+            }
+
+            void HandleE() { }
+            void HandleE(int k) { }
+        }
+
+        class Safety : Monitor
+        {
+            [Start]
+            [OnEntry(nameof(InitOnEntry))]
+            [OnEventDoAction(typeof(E), nameof(HandleE))]
+            public class Init : MonitorState { }
+
+            void InitOnEntry()
+            {
+                this.Raise(new E());
+            }
+
+            void HandleE() { }
+            void HandleE(int k) { }
+        }
+
+        [Fact]
+        public void TestAmbiguousMachineEventHandler()
+        {
+            var test = new Action<PSharpRuntime>((r) => {
+                r.CreateMachine(typeof(Program));
+            });
+
+            base.AssertSucceeded(test);
+        }
+
+        [Fact]
+        public void TestAmbiguousMonitorEventHandler()
+        {
+            var test = new Action<PSharpRuntime>((r) => {
+                r.RegisterMonitor(typeof(Safety));
+            });
+
+            base.AssertSucceeded(test);
+        }
+    }
+}
