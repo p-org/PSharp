acc5dece6625b1507cd32e952f534c43eadcfcb8
Paul Thomson <paul.thomson11@imperial.ac.uk>
2017-07-11 14:37:43 +0100

    Added IContract and ContractAsserter for scheduling strategies.

---
+++ Source/SchedulingStrategies/Utilities/IContract.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IContract.cs">
+//      Copyright (c) Microsoft Corporation. All rights reserved.
+// 
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//      IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//      CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//      TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//      SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Microsoft.PSharp.TestingServices.SchedulingStrategies
+{
+    /// <summary>
+    /// Interface for a contract that can be used to assert that
+    /// a condition in a scheduling strategy should be true.
+    /// </summary>
+    public interface IContract
+    {
+        /// <summary>
+        /// Assert a condition that should be true.
+        /// </summary>
+        /// <param name="condition">The condition.</param>
+        /// <param name="msg">An error message if the condition is false.</param>
+        void Assert(bool condition, string msg = "");
+    }
+}
