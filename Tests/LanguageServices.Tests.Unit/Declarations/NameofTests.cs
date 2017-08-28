df4ad99313b66e36af0efdf3a07b30e69dd9315e
TedHartMS <tedhar@microsoft.com>
2017-08-28 01:02:35 -0700

    Resolve #236: Add Configuration.RewriteCSharpVersion; replace nameof with quoted string if this is < 6.0

---
+++ Tests/LanguageServices.Tests.Unit/Declarations/NameofTests.cs
@@ -0,0 +1,147 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="NameofTests.cs">
+//      Copyright (c) Microsoft Corporation. All rights reserved.
+// 
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//      IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//      CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//      TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//      SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Xunit;
+
+namespace Microsoft.PSharp.LanguageServices.Tests.Unit
+{
+    public class NameofTests
+    {
+        string NameofTest = @"
+namespace Foo {
+    machine M_with_nameof {
+        start state Init
+        {
+            entry
+            {
+                Value += 1;
+                raise(E1);
+            }
+            exit { Value += 10; }
+            on E1 goto Next with { Value += 100; }
+        }
+        
+        state Next
+        {
+            entry
+            {
+                Value += 1000;
+                raise(E2);
+            }
+            on E2 do { Value += 10000; } 
+        }
+    }
+}";
+
+        [Fact]
+        public void TestAllNameofRewriteWithNameof()
+        {
+            var test = NameofTest;
+            var expected = @"
+using Microsoft.PSharp;
+
+namespace Foo
+{
+    class M_with_nameof : Machine
+    {
+        [Microsoft.PSharp.Start]
+        [OnEntry(nameof(psharp_Init_on_entry_action))]
+        [OnExit(nameof(psharp_Init_on_exit_action))]
+        [OnEventGotoState(typeof(E1), typeof(Next), nameof(psharp_Init_E1_action))]
+        class Init : MachineState
+        {
+        }
+
+        [OnEntry(nameof(psharp_Next_on_entry_action))]
+        [OnEventDoAction(typeof(E2), nameof(psharp_Next_E2_action))]
+        class Next : MachineState
+        {
+        }
+
+        protected void psharp_Init_on_entry_action()
+        {
+            Value += 1;
+            this.Raise(new E1());
+        }
+
+        protected void psharp_Init_on_exit_action()
+        { Value += 10; }
+
+        protected void psharp_Next_on_entry_action()
+        {
+            Value += 1000;
+            this.Raise(new E2());
+        }
+
+        protected void psharp_Init_E1_action()
+        { Value += 100; }
+
+        protected void psharp_Next_E2_action()
+        { Value += 10000; }
+    }
+}";
+            LanguageTestUtilities.AssertRewritten(expected, test);
+        }
+
+        [Fact]
+        public void TestAllNameofRewriteWithoutNameof()
+        {
+            var test = NameofTest;
+            var expected = @"
+using Microsoft.PSharp;
+
+namespace Foo
+{
+    class M_with_nameof : Machine
+    {
+        [Microsoft.PSharp.Start]
+        [OnEntry(""psharp_Init_on_entry_action"")]
+        [OnExit(""psharp_Init_on_exit_action"")]
+        [OnEventGotoState(typeof(E1), typeof(Next), ""psharp_Init_E1_action"")]
+        class Init : MachineState
+        {
+        }
+
+        [OnEntry(""psharp_Next_on_entry_action"")]
+        [OnEventDoAction(typeof(E2), ""psharp_Next_E2_action"")]
+        class Next : MachineState
+        {
+        }
+
+        protected void psharp_Init_on_entry_action()
+        {
+            Value += 1;
+            this.Raise(new E1());
+        }
+
+        protected void psharp_Init_on_exit_action()
+        { Value += 10; }
+
+        protected void psharp_Next_on_entry_action()
+        {
+            Value += 1000;
+            this.Raise(new E2());
+        }
+
+        protected void psharp_Init_E1_action()
+        { Value += 100; }
+
+        protected void psharp_Next_E2_action()
+        { Value += 10000; }
+    }
+}";
+            LanguageTestUtilities.AssertRewritten(expected, test, csVersion: new System.Version(5, 0));
+        }
+    }
+}
---
+++ Tests/TestingServices.Tests.Unit/Machines/Declarations/NameofTest.cs
@@ -0,0 +1,127 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="NameofTest.cs">
+//      Copyright (c) Microsoft Corporation. All rights reserved.
+// 
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//      IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//      CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//      TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//      SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+
+using Xunit;
+
+namespace Microsoft.PSharp.TestingServices.Tests.Unit
+{
+    public class NameofTest : BaseTest
+    {
+        static int WithNameofValue;
+        static int WithoutNameofValue;
+
+        class E1 : Event { }
+        class E2 : Event { }
+
+        class M_With_nameof : Machine
+        {
+            [Microsoft.PSharp.Start]
+            [OnEntry(nameof(psharp_Init_on_entry_action))]
+            [OnExit(nameof(psharp_Init_on_exit_action))]
+            [OnEventGotoState(typeof(E1), typeof(Next), nameof(psharp_Init_E1_action))]
+            class Init : MachineState
+            {
+            }
+
+            [OnEntry(nameof(psharp_Next_on_entry_action))]
+            [OnEventDoAction(typeof(E2), nameof(psharp_Next_E2_action))]
+            class Next : MachineState
+            {
+            }
+
+            protected void psharp_Init_on_entry_action()
+            {
+                WithNameofValue += 1;
+                this.Raise(new E1());
+            }
+
+            protected void psharp_Init_on_exit_action()
+            { WithNameofValue += 10; }
+
+            protected void psharp_Next_on_entry_action()
+            {
+                WithNameofValue += 1000;
+                this.Raise(new E2());
+            }
+
+            protected void psharp_Init_E1_action()
+            { WithNameofValue += 100; }
+
+            protected void psharp_Next_E2_action()
+            { WithNameofValue += 10000; }
+        }
+
+        class M_Without_nameof : Machine
+        {
+            [Microsoft.PSharp.Start]
+            [OnEntry("psharp_Init_on_entry_action")]
+            [OnExit("psharp_Init_on_exit_action")]
+            [OnEventGotoState(typeof(E1), typeof(Next), "psharp_Init_E1_action")]
+            class Init : MachineState
+            {
+            }
+
+            [OnEntry("psharp_Next_on_entry_action")]
+            [OnEventDoAction(typeof(E2), "psharp_Next_E2_action")]
+            class Next : MachineState
+            {
+            }
+
+            protected void psharp_Init_on_entry_action()
+            {
+                WithoutNameofValue += 1;
+                this.Raise(new E1());
+            }
+
+            protected void psharp_Init_on_exit_action()
+            { WithoutNameofValue += 10; }
+
+            protected void psharp_Next_on_entry_action()
+            {
+                WithoutNameofValue += 1000;
+                this.Raise(new E2());
+            }
+
+            protected void psharp_Init_E1_action()
+            { WithoutNameofValue += 100; }
+
+            protected void psharp_Next_E2_action()
+            { WithoutNameofValue += 10000; }
+        }
+
+        [Fact]
+        public void TestAllNameofWithNameof()
+        {
+            var test = new Action<PSharpRuntime>((r) => {
+                r.CreateMachine(typeof(M_With_nameof));
+            });
+
+            base.AssertSucceeded(test);
+            Assert.Equal(WithNameofValue, 11111);
+        }
+
+        [Fact]
+        public void TestAllNameofWithoutNameof()
+        {
+            var test = new Action<PSharpRuntime>((r) => {
+                r.CreateMachine(typeof(M_Without_nameof));
+            });
+
+            base.AssertSucceeded(test);
+            Assert.Equal(WithoutNameofValue, 11111);
+        }
+    }
+}
