b7853199149e18ee2e0f88ead4c85e0a2f62f196
Anirudh Santhiar <t-ansant@microsoft.com>
2017-09-22 12:31:47 +0530

    Provides a logger that lets clients queue log messages and writes them out asynchronously.

---
+++ Source/Core/IO/Logging/SyncWriterLogger.cs
@@ -0,0 +1,74 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SyncWriterLogger.cs">
+//      Copyright (c) Microsoft Corporation. All rights reserved.
+// 
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//      IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//      CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//      TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//      SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.IO;
+
+namespace Microsoft.PSharp.IO
+{
+    /// <summary>
+    /// Logger that wraps the TextWriter passed in a thread-safe wrapper
+    /// </summary>
+    internal sealed class SyncWriterLogger : StateMachineLogger
+    {
+
+        TextWriter writer;
+
+        
+        public SyncWriterLogger(TextWriter t)
+        {            
+            writer = t?? TextWriter.Null;
+        }
+        
+        public override void Write(string value)
+        {
+            lock (writer)
+            {
+                writer.Write(value);
+            }
+        }
+
+        
+        public override void Write(string format, params object[] args)
+        {
+            lock (writer)
+            {
+                writer.Write(format, args);
+            }
+        }
+
+        
+        public override void WriteLine(string value)
+        {
+            lock (writer)
+            {
+                writer.WriteLine(value);
+            }
+        }
+
+        
+        public override void WriteLine(string format, params object[] args)
+        {
+            lock (writer)
+            {
+                writer.WriteLine(format, args);
+            }
+        }
+
+        public override void Dispose()
+        {
+            
+        }
+    }
+}
--- Tests/Core.Tests.Performance/Tests/LoggerTest.cs
+++ Tests/Core.Tests.Performance/Tests/LoggerTest.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="MailboxTest.cs">
+// <copyright file="Logger.cs">
 //      Copyright (c) Microsoft Corporation. All rights reserved.
 // 
 //      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
@@ -17,19 +17,35 @@ using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using System.Collections.Concurrent;
 using Microsoft.PSharp.IO;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Engines;
+using System.IO;
+using System;
+using System.Reflection;
+using System.Linq;
 
 namespace Microsoft.PSharp.Core.Tests.Performance
 {
+    /// <summary>
+    /// Warning: this test requires ~1.5 GB of free disk space to run
+    /// It generates a log file in the bin directory
+    /// In the interest of diagnostics, it chooses not to delete the generated log file
+    /// </summary>
     [Config(typeof(Configuration))]
+    [SimpleJob(RunStrategy.Monitoring)]
     public class LoggerTest
     {
         public partial class SimpleMachine
         {
             private PSharpRuntime instance;
-            
+            private TaskCompletionSource<bool> tcs;
+            //private string dictName = "SomeDictionary";
+
             public SimpleMachine()
-            {                
+            {
+                //this.Name = "SimpleMachine";
                 this.Counter = 0;
+                this.targetRounds = 0;
             }
 
             public void SendMessageToPSharp()
@@ -37,7 +53,7 @@ namespace Microsoft.PSharp.Core.Tests.Performance
 
                 Task.Factory.StartNew(async () =>
                 {
-                    await Task.Delay(0);
+                    await Task.Delay(0/*TimeSpan.FromSeconds(10)*/);
                     this.instance.SendEvent(this.Id, new Timeout());
                 });
             }
@@ -52,11 +68,15 @@ namespace Microsoft.PSharp.Core.Tests.Performance
         public class SetContextMessage : Event
         {
             public PSharpRuntime runtime;
+            public int rounds;
+            public TaskCompletionSource<bool> tcs;
 
-            public SetContextMessage(PSharpRuntime runtime)
+            public SetContextMessage(PSharpRuntime runtime, int rounds, TaskCompletionSource<bool> tcs)
                 : base()
             {
                 this.runtime = runtime;
+                this.rounds = rounds;
+                this.tcs = tcs;
             }
         }
 
@@ -73,6 +93,8 @@ namespace Microsoft.PSharp.Core.Tests.Performance
             //ISimpleMachineContext context;
             //string Name;
             int Counter;
+            private int targetRounds;
+            private int rounds;
 
             [Microsoft.PSharp.Start]
             [OnEntry(nameof(InitOnEntry))]
@@ -95,7 +117,10 @@ namespace Microsoft.PSharp.Core.Tests.Performance
             void InitOnEntry()
             {
                 this.Counter = 0;
-                this.instance = (this.ReceivedEvent as SetContextMessage).runtime;
+                var e = this.ReceivedEvent as SetContextMessage;
+                this.instance = e.runtime;
+                this.targetRounds = e.rounds;
+                this.tcs = e.tcs;
                 this.SendMessageToPSharp();
                 this.Goto<SimpleThinking>();
             }
@@ -103,7 +128,7 @@ namespace Microsoft.PSharp.Core.Tests.Performance
             void IncrementCounter()
             {
                 this.Counter++;
-
+                
                 this.SendMessageToPSharp();
 
                 if (this.Counter == 10)
@@ -124,6 +149,13 @@ namespace Microsoft.PSharp.Core.Tests.Performance
                 {
                     for (int i = 0; i < 500; i++) ;
                 }
+                this.rounds++;
+
+                if(this.rounds == this.targetRounds)
+                {
+                    this.Raise(new Halt());
+                    this.tcs.SetResult(true);                                        
+                }
 
                 this.SendMessageToPSharp();
             }
@@ -132,6 +164,10 @@ namespace Microsoft.PSharp.Core.Tests.Performance
             {
                 if (this.Counter == 10)
                 {
+                    // Print to screen
+                    //context.EventSource.Message("{0}: Inside Advance Thinking", this.Name);
+
+                    // Persist
                     this.PersistInline();
                 }
 
@@ -139,24 +175,103 @@ namespace Microsoft.PSharp.Core.Tests.Performance
             }
         }
 
-        [Params(1, 5, 10)]
+        [Params(100, 1000, 10000)]
         public int Clients { get; set; }
 
-        [Benchmark]
-        public void RunWithLogger()
+        [Params(8)]
+        public int TargetRounds { get; set; }
+
+        
+        private string path;
+        private string basePath;
+
+
+        [GlobalSetup]
+        public void GlobalSetup()
         {
-            var configuration = PSharp.Configuration.Create().WithVerbosityEnabled(0);
-            var runtime = new StateMachineRuntime(configuration);            
-            ConcurrentQueue<MachineId> machines = new ConcurrentQueue<MachineId>();
+            basePath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            path = basePath + String.Format(@"\\LoggerTestlog.txt");
+        }
+              
+        [Benchmark(Baseline = true)]
+        public void RunSyncNull()
+        {
+            using (var logger = new SyncWriterLogger(null))
+            {
+                var configuration = PSharp.Configuration.Create().WithVerbosityEnabled(2);
+                var runtime = new StateMachineRuntime(configuration);
+                runtime.SetLogger(logger);
+                ConcurrentQueue<MachineId> machines = new ConcurrentQueue<MachineId>();
+                RunMachine(runtime, machines);
+            }
+        }
+
+        [Benchmark]
+        public void RunAsyncNull()
+        {
+            using (var logger = new AsyncLogger(null))
+            {
+                var configuration = PSharp.Configuration.Create().WithVerbosityEnabled(2);
+                var runtime = new StateMachineRuntime(configuration);
+                runtime.SetLogger(logger);
+                ConcurrentQueue<MachineId> machines = new ConcurrentQueue<MachineId>();
+                RunMachine(runtime, machines);
+            }
+        }
+
+        [Benchmark]
+        public void RunSync()
+        {
+            using (var writer = new StreamWriter(path, false))
+            {
+                using (var logger = new SyncWriterLogger(writer))
+                {
+                    var configuration = PSharp.Configuration.Create().WithVerbosityEnabled(2);
+                    var runtime = new StateMachineRuntime(configuration);
+                    runtime.SetLogger(logger);
+                    ConcurrentQueue<MachineId> machines = new ConcurrentQueue<MachineId>();
+                    RunMachine(runtime, machines);
+                }
+            }
+        }
+
+        [Benchmark]
+        public void RunAsync()
+        {
+            using (var writer = new StreamWriter(path, false))
+            {
+                using (var logger = new AsyncLogger(writer))
+                {
+                    var configuration = PSharp.Configuration.Create().WithVerbosityEnabled(2);
+                    var runtime = new StateMachineRuntime(configuration);
+                    runtime.SetLogger(logger);
+                    ConcurrentQueue<MachineId> machines = new ConcurrentQueue<MachineId>();
+                    RunMachine(runtime, machines);
+                }
+            }
+        }
+
+        private void RunMachine(StateMachineRuntime runtime, ConcurrentQueue<MachineId> machines)
+        {
+            var tcsArray = InitializeArray<TaskCompletionSource<bool>>(Clients);
             Parallel.For(0, Clients, index =>
             {
-                machines.Enqueue(runtime.CreateMachine(typeof(SimpleMachine), new SetContextMessage(runtime)));
+                machines.Enqueue(runtime.CreateMachine(typeof(SimpleMachine), new SetContextMessage(runtime, TargetRounds, tcsArray[index])));
             });
 
-            foreach (var machine in machines)
+            var tasks = from x in tcsArray select x.Task;
+            Task.WhenAll(tasks).Wait();
+        }
+
+        T[] InitializeArray<T>(int length) where T : new()
+        {
+            T[] array = new T[length];
+            for (int i = 0; i < length; ++i)
             {
-                runtime.SendEvent(machine, new Halt());
+                array[i] = new T();
             }
-        }        
+
+            return array;
+        }
     }
 }
