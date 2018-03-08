9e678d7a0a6c6fa8f063c6c3c48cfbdcccaa6b25
Suvam Mukherjee <t-sumukh@microsoft.com>
2018-03-08 22:47:46 +0530

    Completed timer design

---
+++ Source/Core/Timers/Events/Markup.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.PSharp.Timers
+{
+	class Markup : Event { }
+}
