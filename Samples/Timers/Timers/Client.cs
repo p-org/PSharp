dd8310546c54592768406dcccd336da9206941a3
Suvam Mukherjee <t-sumukh@microsoft.com>
2018-03-08 16:54:49 +0530

    Incremental commit

---
+++ Samples/Timers/Timers/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.PSharp;
+using Microsoft.PSharp.Timers;
+
+namespace Timers
+{
+    class Client : Timer
+	{
+		#region states
+		[Start]
+		[OnEntry(nameof(InitializeClient))]
+		[OnEventDoAction(typeof(eTimeout), nameof(HandleTimeout))]
+		class Init : MachineState { }
+
+		#endregion
+
+		#region handlers
+		private void InitializeClient()
+		{
+			Timer.IsTestingMode = true;
+			Timer.IsPeriodic = true;
+			this.Start();
+		}
+
+		private void HandleTimeout()
+		{
+			Console.WriteLine("Timeout received!");
+		}
+		#endregion
+	}
+}
