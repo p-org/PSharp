74fd6591c7fd33100ab9fa0db52e5889554f9a16
Suvam Mukherjee <t-sumukh@microsoft.com>
2018-03-09 11:22:12 +0530

    Adding samples

--- Source/Core/Timers/Events/InitTimer.cs
+++ Source/Core/Timers/Events/InitTimer.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="InitTimer.cs">
+//      Copyright (c) Microsoft Corporation. All rights reserved.
+// 
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//      IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//      CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//      TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//      SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +20,18 @@ using System.Threading.Tasks;
 
 namespace Microsoft.PSharp.Timers
 {
+	/// <summary>
+	/// Event which initializes the model timer.
+	/// </summary>
     class InitTimer : Event
     {
 		public MachineId client;
+		public bool IsPeriodic;
 
-		public InitTimer(MachineId client)
+		public InitTimer(MachineId client, bool IsPeriodic)
 		{
 			this.client = client;
+			this.IsPeriodic = IsPeriodic;
 		}
 	}
 }
