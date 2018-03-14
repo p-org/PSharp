158b5772b461f250e15327a59057014513c600e9
Suvam Mukherjee <t-sumukh@microsoft.com>
2018-03-14 16:22:14 +0530

    Added timer sample

---
+++ Samples/Timers/TimerSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.PSharp;
+
+namespace TimerSample
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			var configuration = Configuration.Create().WithVerbosityEnabled(1);
+			configuration.EnableMonitorsInProduction = true;
+			var runtime = PSharpRuntime.Create(configuration);
+			Execute(runtime);
+			Console.ReadLine();
+		}
+
+		[Microsoft.PSharp.Test]
+		public static void Execute(PSharpRuntime runtime)
+		{
+			/*
+			 * By default, StartTimer would create a machine of type TimerProduction, when running in production mode,
+			 * and a machine of type TimerModel, when running in test. You can change the type of the timer using:
+			 * 
+			 * runtime.SetTimerMachineType(<YourTimerMachineType>);
+			 * 
+			 * Currently, YourTimerMachineType must be a subclass of Machine.
+			 * 
+			*/
+			runtime.CreateMachine(typeof(Client));
+		}
+	}
+}
