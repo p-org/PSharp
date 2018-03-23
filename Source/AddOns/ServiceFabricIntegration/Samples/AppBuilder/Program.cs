dc29019dd073720623ee8ea70329d4470d228ede
Suvam Mukherjee <t-sumukh@microsoft.com>
2018-03-23 17:22:27 +0530

    App Builder started

--- Source/AddOns/ServiceFabricIntegration/Samples/AppBuilder/Program.cs
+++ Source/AddOns/ServiceFabricIntegration/Samples/AppBuilder/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.PSharp;
+using Microsoft.PSharp.ReliableServices;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
 
 namespace AppBuilder
 {
@@ -10,6 +15,31 @@ namespace AppBuilder
 	{
 		static void Main(string[] args)
 		{
+			System.Diagnostics.Debugger.Launch();
+
+			var config = Configuration.Create(); //.WithVerbosityEnabled(2);
+			var runtime = PSharpRuntime.Create(config);
+			runtime.OnFailure += Runtime_OnFailure;
+			var stateManager = new StateManagerMock(runtime);
+			runtime.AddMachineFactory(new ReliableStateMachineFactory(stateManager));
+
+			Console.ReadLine();
+		}
+
+		private static void Runtime_OnFailure(Exception ex)
+		{
+			Console.WriteLine("Runtime failure: {0}", ex.ToString());
+			Console.Out.Flush();
+			Environment.Exit(1);
+		}
+
+		[Test]
+		public static void Execute(PSharpRuntime runtime)
+		{
+			runtime.AddMachineFactory(new ReliableStateMachineFactory(new StateManagerMock(runtime), true));
+			// runtime.RegisterMonitor(typeof(SafetyMonitor));
+			// runtime.CreateMachine(typeof(ClientMachine));
 		}
 	}
+
 }
