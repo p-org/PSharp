78e470e78438c272fd978649935e84d0bc6e09d5
Akash Lal <akashl@microsoft.com>
2018-03-26 16:55:37 +0530

    minor changes

--- Source/AddOns/ReliableServices/ReliableStateMachines/RsmEvents.cs
+++ Source/AddOns/ReliableServices/ReliableStateMachines/RsmEvents.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Microsoft.PSharp.ReliableServices
 {
+    /// <summary>
+    /// RSM Initialization event
+    /// </summary>
+    [DataContract]
     public class RsmInitEvent : Event
     {
-        public readonly RsmHost Host;
+        /// <summary>
+        /// The host
+        /// </summary>
+        public RsmHost Host { get; internal set; }
 
+        /// <summary>
+        /// Initializes a new RsmInitEvent
+        /// </summary>
+        /// <param name="host"></param>
         public RsmInitEvent(RsmHost host)
         {
             this.Host = host;
