4745b5df9e0f17fe9c7a8d3250d95a6e010f6578
Chandramouleswaran Ravichandran <chandramouleswaran@gmail.com>
2018-07-13 13:47:04 -0700

    Resource manager service

---
+++ Source/AddOns/ServiceFabric/ResourceManager/Contracts/generate_protos.bat
@@ -0,0 +1,11 @@
+﻿
+set errorlevel=
+
+cd /d %~dp0
+IF NOT EXIST Codegen MKDIR Codegen
+
+set PROTOC=..\..\..\..\..\packages\Google.Protobuf.Tools\3.6.0\tools\windows_x64\protoc.exe
+set PLUGIN=..\..\..\..\..\packages\Grpc.Tools\1.13.0\tools\windows_x64\grpc_csharp_plugin.exe
+
+%PROTOC% -I %~dp0 --csharp_out Codegen ResourceManager.proto --grpc_out Codegen --plugin=protoc-gen-grpc=%PLUGIN%
+exit /b %errorlevel%
