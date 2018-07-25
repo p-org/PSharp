47d40598c42ad78ab709dac255258c68976ec59a
Suvam Mukherjee <suvamm@outlook.com>
2018-07-25 21:34:20 +0530

    incremental commit

---
+++ Source/AddOns/ServiceFabric/Samples/DemoApp/DemoAppConsole/ClientMachine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.PSharp;
+using Microsoft.PSharp.ServiceFabric;
+using Microsoft.PSharp.ServiceFabric.Utilities;
+using Microsoft.ServiceFabric.Data;
+
+namespace DemoAppConsole
+{
+    // A mock of the client service, fires requests to the driver
+    class ClientMachine : ReliableMachine
+    {
+        public ClientMachine(IRe)
+    }
+}
