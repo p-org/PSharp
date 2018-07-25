6f0f933e0b39a363626af5361e9c51428908232a
Suvam Mukherjee <suvamm@outlook.com>
2018-07-25 11:18:49 +0530

    Added some samples. (#324)
    * WordCount sample added
    
    * fixes to WordCount
    
    * WordCount sample added.
    
    * BankAccount sample added.

---
+++ Source/AddOns/ServiceFabric/Samples/BankAccount/ClientMachine.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.PSharp;
+using Microsoft.PSharp.ServiceFabric;
+using Microsoft.PSharp.ServiceFabric.Utilities;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+
+namespace BankAccount
+{
+    class ClientMachine : ReliableMachine
+    {
+        public ClientMachine(IReliableStateManager stateManager)
+           : base(stateManager)
+        { }
+
+        [Start]
+        [OnEntry(nameof(InitOnEntry))]
+        class Init : MachineState { }
+
+        private void InitOnEntry()
+        {
+            var acc1 = CreateMachine(typeof(AccountMachine), new InitializeAccountEvent("A", 100));
+            var acc2 = CreateMachine(typeof(AccountMachine), new InitializeAccountEvent("B", 100));
+
+            var amount = this.RandomInteger(150);
+
+            var ev = new InitializeBrokerEvent(null, acc1, acc2, amount);
+
+            this.Monitor<SafetyMonitor>(ev);
+            this.Monitor<SafetyMonitor>(new AccountBalanceUpdatedEvent(acc1, 100));
+            this.Monitor<SafetyMonitor>(new AccountBalanceUpdatedEvent(acc2, 100));
+
+            var broker = CreateMachine(typeof(BrokerMachine), ev);
+
+            Send(acc2, new EnableEvent());
+            Send(acc1, new EnableEvent());
+
+            // TODO: Raising a halt is causing the ClientMachine to stop, and does not allow any of the other created machines to continue.
+            //this.Raise(new Halt());
+        }
+
+        protected override Task OnActivate()
+        {
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ Source/AddOns/ServiceFabric/Samples/WordCount/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.PSharp;
+using Microsoft.PSharp.ServiceFabric;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Debugger.Launch();
+
+            var stateManager = new StateManagerMock(null);
+            stateManager.DisallowFailures();
+
+            var config = Configuration.Create();
+            var runtime = ServiceFabricRuntimeFactory.Create(stateManager, config);
+            runtime.OnFailure += Runtime_OnFailure;
+
+            runtime.CreateMachine(typeof(ClientMachine));
+
+            Console.ReadLine();
+        }
+
+        
+        [Test]
+        public static void Execute(PSharpRuntime runtime)
+        {
+            runtime.RegisterMonitor(typeof(SafetyMonitor));
+            runtime.CreateMachine(typeof(ClientMachine));
+        }
+        
+        private static void Runtime_OnFailure(Exception ex)
+        {
+            Console.WriteLine("Exception in the runtime: {0}", ex.Message);
+            System.Diagnostics.Debugger.Launch();
+        }
+    }
+
+    static class Config
+    {
+        public static readonly int NumMachines = 2;
+        public static readonly int StringLen = 2;
+        public static readonly int NumWords = 3;
+    }
+
+}
