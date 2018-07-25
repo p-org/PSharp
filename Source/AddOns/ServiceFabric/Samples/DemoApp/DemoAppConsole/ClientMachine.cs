57a0f19bbf6d7e7dc1885d365fb04c47ddf5225e
Suvam Mukherjee <suvamm@outlook.com>
2018-07-25 23:00:03 +0530

    added ClientMachine

--- Source/AddOns/ServiceFabric/Samples/DemoApp/DemoAppConsole/ClientMachine.cs
+++ Source/AddOns/ServiceFabric/Samples/DemoApp/DemoAppConsole/ClientMachine.cs
@@ -7,12 +7,66 @@ using Microsoft.PSharp;
 using Microsoft.PSharp.ServiceFabric;
 using Microsoft.PSharp.ServiceFabric.Utilities;
 using Microsoft.ServiceFabric.Data;
+using PoolServicesContract;
 
 namespace DemoAppConsole
 {
     // A mock of the client service, fires requests to the driver
     class ClientMachine : ReliableMachine
     {
-        public ClientMachine(IRe)
+        #region fields
+        
+        // Handle to the driver machine
+        ReliableRegister<MachineId> PoolDriver;
+
+        #endregion
+
+        public ClientMachine(IReliableStateManager stateManager)
+             : base(stateManager)
+        { }
+
+        #region states
+
+        [Start]
+        [OnEntry(nameof(InitClient))]
+        class Init : MachineState { }
+
+        #endregion
+
+        #region handlers
+        private async Task InitClient()
+        {
+            eInitClient ev = this.ReceivedEvent as eInitClient;
+
+            await PoolDriver.Set(ev.driver);
+
+            // Setup a config
+            ePoolDriverConfigChangeEvent evConfigChange = new ePoolDriverConfigChangeEvent();
+            evConfigChange.Configuration = new PoolDriverConfig();
+            evConfigChange.Configuration.PoolData = new Dictionary<string, int>();
+
+            // Fire off some pool creation/resize requests to the driver
+            evConfigChange.Configuration.PoolData.Add("Pool1", 10);
+            Send(await PoolDriver.Get(), evConfigChange);
+
+            // Scale up
+            evConfigChange.Configuration.PoolData.Remove("Pool1");
+            evConfigChange.Configuration.PoolData.Add("Pool1", 20);
+            Send(await PoolDriver.Get(), evConfigChange);
+
+            // Scale down
+            evConfigChange.Configuration.PoolData.Remove("Pool1");
+            evConfigChange.Configuration.PoolData.Add("Pool1", 15);
+            Send(await PoolDriver.Get(), evConfigChange);
+        }
+        #endregion
+
+        protected override Task OnActivate()
+        {
+            PoolDriver = this.GetOrAddRegister<MachineId>("PoolDriver", null);
+            return Task.CompletedTask;
+        }
+
+
     }
 }
