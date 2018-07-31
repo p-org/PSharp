54755ce84c1583b030dc93af0059104daf8a2071
Akash Lal <akashl@microsoft.com>
2018-07-31 12:42:37 +0530

    Outbox per machine

--- Source/AddOns/ServiceFabric/Runtime/StateManagerExtensions.cs
+++ Source/AddOns/ServiceFabric/Runtime/StateManagerExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.ServiceFabric.Data;
 using Microsoft.ServiceFabric.Data.Collections;
+using System;
 using System.Threading.Tasks;
 
 namespace Microsoft.PSharp.ServiceFabric
@@ -7,6 +8,7 @@ namespace Microsoft.PSharp.ServiceFabric
     public static class StateManagerExtensions
     {
         private const string InputQueuePrefix = "InputQueue_";
+        private const string OutputQueuePrefix = "OutputQueue_";
         private const string SendCountersPrefix = "SendCounters_";
         private const string ReceiveCountersPrefix = "ReceiveCounters_";
         private const string StateStackStorePrefix = "StateStackStore_";
@@ -19,6 +21,16 @@ namespace Microsoft.PSharp.ServiceFabric
         public static Task DeleteMachineInputQueue(this IReliableStateManager manager, MachineId id)
         {
             return manager.RemoveAsync(InputQueuePrefix + id.ToString());
+        }
+
+        internal static Task<IReliableQueue<OutboxElement>> GetMachineOutputQueue(this IReliableStateManager manager, MachineId id)
+        {
+            return manager.GetOrAddAsync<IReliableQueue<OutboxElement>> (OutputQueuePrefix + id.ToString());
+        }
+
+        public static Task DeleteMachineOutputQueue(this IReliableStateManager manager, MachineId id)
+        {
+            return manager.RemoveAsync(OutputQueuePrefix + id.ToString());
         }
 
         public static Task<IReliableDictionary<string, int>> GetMachineSendCounters(this IReliableStateManager manager, MachineId id)
