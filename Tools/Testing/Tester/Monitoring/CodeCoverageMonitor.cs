7cecea14866d78b5add2b7549ac82ffe140e471b
Pantazis Deligiannis <pdeligia@me.com>
2018-09-06 14:27:24 +0100

    simplified license info in files (#350)

--- Tools/Testing/Tester/Monitoring/CodeCoverageMonitor.cs
+++ Tools/Testing/Tester/Monitoring/CodeCoverageMonitor.cs
@@ -1,17 +1,8 @@
 ﻿#if NET46 || NET45
-//-----------------------------------------------------------------------
-// <copyright file="CodeCoverageMonitor.cs">
-//      Copyright (c) Microsoft Corporation. All rights reserved.
-// 
-//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-//      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-//      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-//      IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-//      CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-//      TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-//      SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// </copyright>
-//-----------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------------------------------------------
 
 using Microsoft.PSharp.IO;
 using System;
