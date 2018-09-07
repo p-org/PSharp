960873657243f85894586526c9f003ca846abfbc
Pantazis Deligiannis <pdeligia@microsoft.com>
2018-09-07 10:23:30 +0100

    improvements in samples build system and structure

---
+++ Samples/Framework/CacheCoherence/Test.cs
@@ -0,0 +1,46 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------------------------------------------
+
+using System;
+using Microsoft.PSharp;
+
+namespace CacheCoherence
+{
+    /// <summary>
+    /// A single-process implementation of the cache coherence protocol by Steven German
+    /// written using P# as a C# library.
+    ///
+    /// An overview of the protocol is described in the following tutorial:
+    /// http://www.cs.utah.edu/~ganesh/presentations/fmcad04_tutorial2/chou/ctchou-tutorial.pdf
+    ///
+    /// Note: this is an abstract implementation aimed primarily to showcase the testing
+    /// capabilities of P#.
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Optional: increases verbosity level to see the P# runtime log.
+            var configuration = Configuration.Create().WithVerbosityEnabled(2);
+
+            // Creates a new P# runtime instance, and passes an optional configuration.
+            var runtime = PSharpRuntime.Create(configuration);
+
+            // Executes the P# program.
+            Program.Execute(runtime);
+
+            // The P# runtime executes asynchronously, so we wait
+            // to not terminate the process.
+            Console.WriteLine("Press Enter to terminate...");
+            Console.ReadLine();
+        }
+
+        [Microsoft.PSharp.Test]
+        public static void Execute(PSharpRuntime runtime)
+        {
+            runtime.CreateMachine(typeof(Host));
+        }
+    }
+}
---
+++ Samples/Framework/FailureDetector/Node.cs
@@ -0,0 +1,52 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using Microsoft.PSharp;
+
+namespace FailureDetector
+{
+    /// <summary>
+    /// Implementation of a simple node.
+    ///
+    /// The node responds with a 'Pong' event whenever it receives
+	/// a 'Ping' event. This is used as a heartbeat to show that
+	/// the node is still alive.
+    /// </summary>
+    internal class Node : Machine
+    {
+        internal class Ping : Event
+        {
+            public MachineId Client;
+
+            public Ping(MachineId client)
+            {
+                this.Client = client;
+            }
+        }
+
+        internal class Pong : Event
+        {
+            public MachineId Node;
+
+            public Pong(MachineId node)
+            {
+                this.Node = node;
+            }
+        }
+
+        [Start]
+        [OnEventDoAction(typeof(Ping), nameof(SendPong))]
+        class WaitPing : MachineState { }
+
+        void SendPong()
+        {
+            var client = (this.ReceivedEvent as Ping).Client;
+            this.Monitor<Safety>(new Safety.Pong(this.Id));
+            this.Send(client, new Pong(this.Id));
+        }
+    }
+}
