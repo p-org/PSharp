fff57e636596085808b77d3a4391b6afe77dd911
Pantazis Deligiannis <pdeligia@microsoft.com>
2018-10-16 15:42:59 +0100

    updates to nuget upload script

--- Scripts/upload-nuget-packages.ps1
+++ Scripts/upload-nuget-packages.ps1
@@ -1,10 +1,10 @@
 param(
     [Parameter(Mandatory=$true)]
-    [string]$account_name="",
+    [string]$account_name,
     [Parameter(Mandatory=$true)]
-    [string]$account_key="",
+    [string]$account_key,
     [Parameter(Mandatory=$true)]
-    [string]$share_name=""
+    [string]$share_name
 )
 
 Import-Module $PSScriptRoot\powershell\common.psm1
@@ -13,12 +13,11 @@ $package_dir = "$PSScriptRoot\..\bin\nuget"
 
 Write-Comment -prefix "." -text "Uploading the P# NuGet packages to Azure Storage" -color "yellow"
 
+$context = New-AzureStorageContext -StorageAccountName $account_name -StorageAccountKey $account_key
+
 $packages = Get-ChildItem "$package_dir"
 foreach ($p in $packages) {
-    Write-Host "$p"
-    $command = "az storage file upload --account-name $account_name --account-key $account_key --share-name $share_name --source $package_dir\$p"
-    $error_msg = "Failed to upload the P# NuGet packages to Azure Storage"
-    Invoke-ToolCommand -tool $nuget -command $command -error_msg $error_msg
+    Set-AzureStorageFileContent  -Context $context -ShareName $share_name -Source "$package_dir\$p" -Path "nuget\$p"
 }
 
 Write-Comment -prefix "." -text "Successfully uploaded the P# NuGet packages to Azure Storage" -color "green"
