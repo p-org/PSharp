bce2de1ceaa854d6dbbab49b94c732cb8cfea0ec
ellab123 <ellab@microsoft.com>
2019-05-07 16:47:46 -0700

    new tests added

--- Tests/Core.Tests/Machines/ReceiveTestsFromP.cs
+++ Tests/Core.Tests/Machines/ReceiveTestsFromP.cs
@@ -67,6 +67,18 @@ namespace Microsoft.PSharp.Core.Tests
             }
         }
 
+        private class J : Event
+        {
+            public int I;
+            public TaskCompletionSource<bool> Tcs;
+
+            public J(int i, TaskCompletionSource<bool> tcs)
+            {
+                this.I = i;
+                this.Tcs = tcs;
+            }
+        }
+
         private class F : Event
         {
             public TaskCompletionSource<bool> Tcs;
@@ -424,6 +436,55 @@ namespace Microsoft.PSharp.Core.Tests
             }
         }
 
+        private class M6 : Machine
+        {
+            [Start]
+            [OnEntry(nameof(InitOnEntry))]
+            [OnEventDoAction(typeof(UnitTcsBool), nameof(OnUnitAction))]
+            private class Init : MachineState
+            {
+            }
+
+            private void InitOnEntry()
+            {
+                var tcs = (this.ReceivedEvent as SetupEventBool).Tcs;
+                var bid = this.CreateMachine(typeof(B6), new Config(this.Id, tcs));
+                this.Raise(new UnitTcsBool(tcs));
+            }
+
+            private async Task OnUnitAction()
+            {
+                Event receivedEvent = await this.Receive(Tuple.Create<Type, Func<Event, bool>>(typeof(G), e => (e as G).I == 1),
+                                                         Tuple.Create<Type, Func<Event, bool>>(typeof(J), e => (e as J).I == 2));
+
+                if (receivedEvent is G eventG)
+                {
+                    eventG.Tcs.SetResult(true);
+                }
+                else if (receivedEvent is J eventJ)
+                {
+                    eventJ.Tcs.SetResult(false);
+                }
+            }
+        }
+
+        private class B6 : Machine
+        {
+            [Start]
+            [OnEntry(nameof(InitOnEntry))]
+            private class Init : MachineState
+            {
+            }
+
+            private void InitOnEntry()
+            {
+                MachineId mid = (this.ReceivedEvent as Config).Id;
+                var tcs = (this.ReceivedEvent as Config).Tcs;
+                this.Send(mid, new G(1, tcs));
+                this.Send(mid, new J(2, tcs));
+            }
+        }
+
         [Fact(Timeout = 5000)]
         // Similar to \P\Tst\RegressionTests\Feature2Stmts\Correct\receive6\receive6.p
         public void TestReceiveEventBlocking1()
@@ -505,6 +566,22 @@ namespace Microsoft.PSharp.Core.Tests
             {
                 var tcs = new TaskCompletionSource<bool>();
                 r.CreateMachine(typeof(M5), new SetupEventBool(tcs));
+                var result = await Task.WhenAny(tcs.Task, Task.Delay(5000));
+                Assert.True(tcs.Task.Result);
+            });
+
+            this.Run(configuration, test);
+        }
+
+        [Fact(Timeout = 5000)]
+        public void TestMultipleReceiveEventsPreds()
+        {
+            var configuration = GetConfiguration();
+            configuration.IsVerbose = true;
+            var test = new Action<IMachineRuntime>(async (r) =>
+            {
+                var tcs = new TaskCompletionSource<bool>();
+                r.CreateMachine(typeof(M6), new SetupEventBool(tcs));
                 var result = await Task.WhenAny(tcs.Task, Task.Delay(5000));
                 Assert.True(tcs.Task.Result);
             });
