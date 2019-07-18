783f629ff887b7a2b7f6bd7946bc68af58336da5
Krishnan Govindraj <t-krgov@microsoft.com>
2019-07-18 23:49:37 +0530

    Add SampleTestingClient

---
+++ Samples/TestingClient/SampleTestingClient/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.PSharp;
+using Microsoft.PSharp.TestingClientInterface;
+using System;
+
+namespace SampleTestingClient
+{
+    public class Program { 
+        public static void Main(string[] args)
+        {
+            if (args.Length < 4)
+            {
+                Microsoft.PSharp.IO.Error.Report($"Usage: SampleTestingClient.exe <AssemblyToBeTested> <nIterations> <maxSteps> <nBugsToStopAt>");
+                Environment.Exit(1);
+            }
+
+            try
+            {
+                AppDomain currentDomain = AppDomain.CurrentDomain;
+                currentDomain.UnhandledException += new UnhandledExceptionEventHandler(UnhandledExceptionHandler);
+                Configuration config = CreateConfig(args);
+                SampleController controller = new SampleController(config, Int32.Parse(args[3]));
+                TestingClient testInterface = new TestingClient(controller);
+                testInterface.Run();
+             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        private static Configuration CreateConfig(string[] args)
+        {
+            Configuration config = AbstractStrategyController.CreateDefaultConfiguration();
+
+            config.AssemblyToBeAnalyzed = args[0];
+            config.SchedulingIterations = Int32.Parse(args[1]);
+            config.MaxSchedulingSteps = Int32.Parse(args[2]);
+            
+            config.PerformFullExploration = true;
+            return config;
+        }
+        /// <summary>
+        /// Handler for unhandled exceptions.
+        /// </summary>
+        private static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            var ex = (Exception)args.ExceptionObject;
+            Microsoft.PSharp.IO.Error.Report("[PSharpTester] internal failure: {0}: {1}", ex.GetType().ToString(), ex.Message);
+            Microsoft.PSharp.IO.Output.WriteLine(ex.StackTrace);
+            Environment.Exit(1);
+        }
+    }
+}
---
+++ Samples/TestingClient/SampleTestingClient/SampleController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.PSharp;
+using Microsoft.PSharp.TestingClientInterface;
+using Microsoft.PSharp.TestingServices.Scheduling.ClientInterface;
+using Microsoft.PSharp.TestingServices.Scheduling.Strategies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTestingClient
+{
+    public class SampleController : AbstractStrategyController
+    {
+        internal ISchedulingStrategy ActiveStrategy;
+        private int BugCount;
+        private int nBugstoStopAt;
+        public SampleController(Configuration configuration, int nBugsToStopAt)
+            : base(configuration)
+        {
+            this.ActiveStrategy = null;
+            this.BugCount = 0;
+
+            this.nBugstoStopAt = nBugsToStopAt;
+        }
+
+        public override string GetReport()
+        {
+            return "SampleController found " + this.BugCount + " bugs";
+        }
+
+        public override void NotifySchedulingEnded(bool bugFound)
+        {
+            if (bugFound)
+            {
+                this.BugCount++;
+            }
+
+            // Does nothing, but you should.
+        }
+
+        // public void SetControlUnitStrategy(ControlUnitStrategy controlUnitStrategy)
+        // {
+        //    this.ControlStrategy = controlUnitStrategy;
+        // }
+
+        public override bool StrategyPrepareForNextIteration(out ISchedulingStrategy nextStrategy, out int maxSteps)
+        {
+            if (this.ActiveStrategy == null)
+            {
+                this.ActiveStrategy = new RandomStrategy(this.Configuration.MaxFairSchedulingSteps);
+            }
+            else
+            {
+                this.ActiveStrategy.PrepareForNextIteration();
+            }
+
+            maxSteps = this.Configuration.MaxFairSchedulingSteps;
+
+            nextStrategy = this.ActiveStrategy;
+            return this.BugCount < this.nBugstoStopAt;
+        }
+
+        public override void StrategyReset()
+        {
+            this.ActiveStrategy.Reset();
+        }
+    }
+}
