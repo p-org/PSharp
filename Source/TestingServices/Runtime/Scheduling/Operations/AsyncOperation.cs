eca2052f265b2ba6b66dc90ae0de88bbaa8863b9
Pantazis Deligiannis <pdeligia@microsoft.com>
2019-08-23 10:31:48 -0700

    removed copyright from files

--- Source/TestingServices/Runtime/Scheduling/Operations/AsyncOperation.cs
+++ Source/TestingServices/Runtime/Scheduling/Operations/AsyncOperation.cs
@@ -1,7 +1,4 @@
 ﻿// ------------------------------------------------------------------------------------------------
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
-// ------------------------------------------------------------------------------------------------
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
--- Tools/Testing/Tester/Interfaces/ITestingProcessScheduler.cs
+++ Tools/Testing/Tester/Interfaces/ITestingProcessScheduler.cs
@@ -1,7 +1,4 @@
 ﻿// ------------------------------------------------------------------------------------------------
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
-// ------------------------------------------------------------------------------------------------
 
 #if NET46
 using System.ServiceModel;
