c4f12a902f0be88448a6536b774cdf10ed90cfc1
Pantazis Deligiannis <pdeligia@microsoft.com>
2019-10-30 01:01:29 +0000

    Update to version 1.6.10 (#473)

--- Scripts/create-nuget-packages.ps1
+++ Scripts/create-nuget-packages.ps1
@@ -1,25 +1,28 @@
-param(
-    [string]$nuget="$PSScriptRoot\NuGet\nuget.exe"
-)
-
 Import-Module $PSScriptRoot\powershell\common.psm1
 
-$nuget_exe_dir = "$PSScriptRoot\NuGet"
-$nuget_exe_url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+Write-Comment -prefix "." -text "Creating the P# NuGet package" -color "yellow"
 
-Write-Comment -prefix "." -text "Creating the P# NuGet packages" -color "yellow"
-if (-not (Test-Path $nuget)) {
-    Write-Comment -prefix "..." -text "Downloading latest 'nuget.exe'" -color "white"
-    Invoke-WebRequest "$nuget_exe_url" -OutFile "$nuget_exe_dir\nuget.exe"
-    if (-not (Test-Path $nuget)) {
-        Write-Error "Unable to download 'nuget.exe'. Please download '$nuget_exe_url' and place in '$nuget_exe_dir\' directory."
-        exit
-    }
-    Write-Comment -prefix "..." -text "Installed 'nuget.exe' in '$nuget_exe_dir'" -color "white"
+# Check that NuGet.exe is installed.
+$nuget = "nuget"
+if (-not (Get-Command $nuget -errorAction SilentlyContinue)) {
+    Write-Comment -text "Please install the latest NuGet.exe from https://www.nuget.org/downloads and add it to the PATH environment variable." -color "yellow"
+    exit 1
 }
 
-$command = "pack $nuget_exe_dir\PSharp.nuspec -OutputDirectory $PSScriptRoot\..\bin\nuget"
-$error_msg = "Failed to create the P# NuGet packages"
+# Extract the package version.
+$version_file = "$PSScriptRoot\..\Common\version.props"
+$version_node = Select-Xml -Path $version_file -XPath "/" | Select-Object -ExpandProperty Node
+$version = $version_node.Project.PropertyGroup.VersionPrefix
+$version_suffix = $version_node.Project.PropertyGroup.VersionSuffix
+
+# Setup the command line options for nuget pack.
+$command_options = "-OutputDirectory $PSScriptRoot\..\bin\nuget -Version $version"
+if ($version_suffix) {
+    $command_options = "$command_options -Suffix $version_suffix"
+}
+
+$command = "pack $PSScriptRoot\NuGet\PSharp.nuspec $command_options"
+$error_msg = "Failed to create the P# NuGet package"
 Invoke-ToolCommand -tool $nuget -command $command -error_msg $error_msg
 
-Write-Comment -prefix "." -text "Successfully created the P# NuGet packages" -color "green"
+Write-Comment -prefix "." -text "Successfully created the P# NuGet package" -color "green"
