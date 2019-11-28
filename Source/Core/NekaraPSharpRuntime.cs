4c8987cdbf2f153ef1c976a46982e5cd53ba97d3
Kumseok Jung <jungkumseok@gmail.com>
2019-11-28 16:49:05 +0530

    added NekaraPSharpRuntime, NekaraRuntime - MachineRuntime instrumented with Nekara API

---
+++ Source/Core/NekaraPSharpRuntime.cs
@@ -0,0 +1,34 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------------------------------------------
+
+using Microsoft.PSharp.Runtime;
+
+namespace Microsoft.PSharp
+{
+    /// <summary>
+    /// The runtime for creating and executing machines.
+    /// </summary>
+    public static class NekaraPSharpRuntime
+    {
+        /// <summary>
+        /// Creates a new runtime.
+        /// </summary>
+        /// <returns>The created runtime.</returns>
+        public static IMachineRuntime Create()
+        {
+            return new NekaraRuntime(Configuration.Create());
+        }
+
+        /// <summary>
+        /// Creates a new runtime with the specified <see cref="Configuration"/>.
+        /// </summary>
+        /// <param name="configuration">The runtime configuration to use.</param>
+        /// <returns>The created runtime.</returns>
+        public static IMachineRuntime Create(Configuration configuration)
+        {
+            return new NekaraRuntime(configuration ?? Configuration.Create());
+        }
+    }
+}
